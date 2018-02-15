f26a8535b4f80a84b46b5149e6d7373bd9b2702c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-02-15 18:18:44 +0100

    removing whitespace [ci-skip]

--- private/clsim/dom/I3PhotonToMCPEConverter.cxx
+++ private/clsim/dom/I3PhotonToMCPEConverter.cxx
@@ -53,7 +53,7 @@
 // The module
 I3_MODULE(I3PhotonToMCPEConverter);
 
-I3PhotonToMCPEConverter::I3PhotonToMCPEConverter(const I3Context& context) 
+I3PhotonToMCPEConverter::I3PhotonToMCPEConverter(const I3Context& context)
 : I3ConditionalModule(context)
 {
     AddParameter("RandomService",
@@ -125,7 +125,7 @@ I3PhotonToMCPEConverter::I3PhotonToMCPEConverter(const I3Context& context)
 
     // add an outbox
     AddOutBox("OutBox");
-    
+
     numGeneratedHits_=0;
 
 }
@@ -162,45 +162,45 @@ void I3PhotonToMCPEConverter::Configure()
 
     if (DOMOversizeFactor_ != DOMPancakeFactor_)
         log_warn("You chose \"DOMOversizeFactor\" and \"DOMPancakeFactor\" to be different. Be sure you know whot you are doing! You probably don't want this.");
-    
+
     if (replaceRelativeDOMEfficiencyWithDefault_)
     {
         if (std::isnan(defaultRelativeDOMEfficiency_))
             log_fatal("You need to set \"DefaultRelativeDOMEfficiency\" to a value other than NaN if you enabled \"ReplaceRelativeDOMEfficiencyWithDefault\"");
     }
 
-    if (defaultRelativeDOMEfficiency_<0.) 
+    if (defaultRelativeDOMEfficiency_<0.)
         log_fatal("The \"DefaultRelativeDOMEfficiency\" parameter must not be < 0!");
-    
+
     if (!wavelengthAcceptance_)
         log_fatal("The \"WavelengthAcceptance\" parameter must not be empty.");
     if (!angularAcceptance_)
         log_fatal("The \"AngularAcceptance\" parameter must not be empty.");
-    
+
     if (!wavelengthAcceptance_->HasNativeImplementation())
         log_fatal("The wavelength acceptance function must have a native (i.e. non-OpenCL) implementation!");
     if (!angularAcceptance_->HasNativeImplementation())
         log_fatal("The angular acceptance function must have a native (i.e. non-OpenCL) implementation!");
-    
-    
+
+
     if (!randomService_) {
         log_info("No random service provided as a parameter, trying to get one from the context..");
         randomService_ = context_.Get<I3RandomServicePtr>();
     }
-    
+
     if (!randomService_)
         log_fatal("No random service provided using the \"RandomService\" parameter (and there is none installed on the context).");
-    
+
 }
 
 void I3PhotonToMCPEConverter::DetectorStatus(I3FramePtr frame)
 {
     log_trace("%s", __PRETTY_FUNCTION__);
-        
+
     I3DetectorStatusConstPtr detectorStatus = frame->Get<I3DetectorStatusConstPtr>("I3DetectorStatus");
     if (!detectorStatus)
         log_fatal("detector status frame does not have an I3DetectorStatus entry");
-    
+
     // store it for later
     status_ = detectorStatus;
 
@@ -210,14 +210,14 @@ void I3PhotonToMCPEConverter::DetectorStatus(I3FramePtr frame)
 void I3PhotonToMCPEConverter::Calibration(I3FramePtr frame)
 {
     log_trace("%s", __PRETTY_FUNCTION__);
-    
+
     I3CalibrationConstPtr calibration = frame->Get<I3CalibrationConstPtr>("I3Calibration");
     if (!calibration)
         log_fatal("calibration frame does not have an I3Calibration entry");
-    
+
     // store it for later
     calibration_ = calibration;
-    
+
     PushFrame(frame);
 }
 
@@ -232,16 +232,16 @@ namespace {
 void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
 {
     log_trace("%s", __PRETTY_FUNCTION__);
-    
+
     // First we need to get our geometry
     I3OMGeoMapConstPtr omgeo = frame->Get<I3OMGeoMapConstPtr>("I3OMGeoMap");
     I3ModuleGeoMapConstPtr modulegeo = frame->Get<I3ModuleGeoMapConstPtr>("I3ModuleGeoMap");
-    
+
     if (!omgeo)
         log_fatal("Missing geometry information! (No \"I3OMGeoMap\")");
     if (!modulegeo)
         log_fatal("Missing geometry information! (No \"I3ModuleGeoMap\")");
-    
+
     if (!replaceRelativeDOMEfficiencyWithDefault_) {
         // no need to check for exitsing calibration frames if the efficiency
         // will be replaced with a default value anyway
@@ -251,17 +251,17 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
 
     if ((!status_) && (ignoreDOMsWithoutDetectorStatusEntry_))
         log_fatal("no DetectorStatus frame yet, but received a Physics frame.");
-    
+
     I3PhotonSeriesMapConstPtr inputPhotonSeriesMap = frame->Get<I3PhotonSeriesMapConstPtr>(inputPhotonSeriesMapName_);
     if (!inputPhotonSeriesMap) {
         log_debug("Frame does not contain an I3PhotonSeriesMap named \"%s\".",
                   inputPhotonSeriesMapName_.c_str());
-        
+
         // do nothing if there is no input data
         PushFrame(frame);
         return;
     }
-    
+
     // currently, the only reason we need the MCTree is that I3MCPE does
     // only allow setting the major/minor particle IDs using an existing
     // I3Particle instance with that ID combination.
@@ -269,12 +269,12 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
 
     // allocate the output hitSeriesMap
     I3MCPESeriesMapPtr outputMCPESeriesMap(new I3MCPESeriesMap());
-    
+
     BOOST_FOREACH(const I3PhotonSeriesMap::value_type &it, *inputPhotonSeriesMap)
     {
         const ModuleKey &module_key = it.first;
         // assume this is IceCube (i.e. one PMT with index 0 per DOM)
-        const OMKey key(module_key.GetString(), module_key.GetOM(), 0);        
+        const OMKey key(module_key.GetString(), module_key.GetOM(), 0);
         const I3PhotonSeries &photons = it.second;
 
         if (ignoreDOMsWithoutDetectorStatusEntry_) {
@@ -282,7 +282,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
             if (om_stat==status_->domStatus.end()) continue; // ignore it
             if (om_stat->second.pmtHV==0.) continue; // ignore pmtHV==0
         }
-        
+
         // Find the current OM in the omgeo map
         I3OMGeoMap::const_iterator geo_it = omgeo->find(key);
         if (geo_it == omgeo->end())
@@ -303,21 +303,21 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
             (std::abs(om.position.GetZ() - module.GetPos().GetZ()) > .01*I3Units::mm))
             log_fatal("Module(%i/%u) has a PMT that is not in the center of the DOM!",
                       module_key.GetString(), module_key.GetOM());
-        
+
         const I3Direction pmtDir = om.GetDirection();
         const I3Direction domDir = module.GetDir();
-        
+
         const double DOMDir_x = pmtDir.GetX();
         const double DOMDir_y = pmtDir.GetY();
         const double DOMDir_z = pmtDir.GetZ();
-        
+
         if ((std::abs(DOMDir_x - domDir.GetX()) > 1e-5) ||
             (std::abs(DOMDir_y - domDir.GetY()) > 1e-5) ||
             (std::abs(DOMDir_z - domDir.GetZ()) > 1e-5))
             log_fatal("PMT and DOM directions are not aligned!");
-        
+
         // Find the current OM in the calibration map
-        
+
         // relative DOM efficiency from calibration
         double efficiency_from_calibration=NAN;
 
@@ -325,7 +325,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
         {
             efficiency_from_calibration=defaultRelativeDOMEfficiency_;
         }
-        else 
+        else
         {
             if (!calibration_) {
                 if (std::isnan(defaultRelativeDOMEfficiency_)) {
@@ -350,11 +350,11 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                 } else {
                     const I3DOMCalibration &domCalibration = cal_it->second;
                     efficiency_from_calibration=domCalibration.GetRelativeDomEff();
-                    
+
                     if (std::isnan(efficiency_from_calibration)) {
                         if (std::isnan(defaultRelativeDOMEfficiency_)) {
                             log_fatal("OM (%i/%u) found in the current calibration map, but it is NaN! (Consider setting \"DefaultRelativeDOMEfficiency\" != NaN)", key.GetString(), key.GetOM());
-                        } else {                
+                        } else {
                             efficiency_from_calibration = defaultRelativeDOMEfficiency_;
                             log_debug("OM (%i/%u): efficiency_from_calibration=%g (default (was: NaN))",
                                       key.GetString(), key.GetOM(),
@@ -368,8 +368,8 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                 }
             }
         }
-        
-        // a pointer to the output vector. The vector will be allocated 
+
+        // a pointer to the output vector. The vector will be allocated
         // by the map, this is merely a pointer to it in case we have multiple
         // hits per OM.
         I3MCPESeries *hits = NULL;
@@ -387,12 +387,12 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
             const double py=om.position.GetY()-photon.GetPos().GetY();
             const double pz=om.position.GetZ()-photon.GetPos().GetZ();
             const double pr2 = px*px + py*py + pz*pz;
-            
+
             double photonCosAngle = -(dx * DOMDir_x +
                                       dy * DOMDir_y +
                                       dz * DOMDir_z);
             photonCosAngle = std::max(-1., std::min(1., photonCosAngle));
-            
+
             const double distFromDOMCenter = std::sqrt(pr2);
 
             // do this only if DOMs are spherical
@@ -433,7 +433,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                     }
                 }
             }
-            
+
             // sanity check for unscattered photons: is their direction ok
             // w.r.t. the vector from emission to detection?
             if (photon.GetNumScattered()==0)
@@ -444,7 +444,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                 const double ppl = std::sqrt(ppx*ppx + ppy*ppy + ppz*ppz);
                 ppx/=ppl; ppy/=ppl; ppz/=ppl;
                 const double cosang = dx*ppx + dy*ppy + dz*ppz;
-                
+
                 if ((cosang < 0.9) && (ppl>1.*I3Units::m)) {
                     log_fatal("unscattered photon direction is inconsistent: cos(ang)==%f, d=(%f,%f,%f), pp=(%f,%f,%f) pp_l=%f",
                               cosang,
@@ -454,7 +454,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                               );
                 }
             }
-            
+
 #ifndef NDEBUG
             const double photonAngle = std::acos(photonCosAngle);
             log_trace("Photon (lambda=%fnm, angle=%fdeg, dist=%fm) has weight %g",
@@ -463,7 +463,7 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                      distFromDOMCenter/I3Units::m,
                      hitProbability);
 #endif
-            
+
             hitProbability *= wavelengthAcceptance_->GetValue(photon.GetWavelength());
             log_trace("After wlen acceptance: prob=%g (wlen acceptance is %f)",
                      hitProbability, wavelengthAcceptance_->GetValue(photon.GetWavelength()));
@@ -500,22 +500,22 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                 hitProbability *= efficiency_from_calibration;
                 log_warn("After efficiency from calibration: prob=%g (efficiency_from_calibration=%f)",
                           hitProbability, efficiency_from_calibration);
-                
+
                 log_fatal("cannot continue.");
             }
-            
+
             // does it survive?
             if (hitProbability <= randomService_->Uniform()) continue;
 
             // find the particle
             const I3Particle *particle = NULL;
-            
+
             if ((photon.GetParticleMajorID() != 0) || (photon.GetParticleMinorID() != 0))
             {
                 // index (0,0) is used for flasher photons, set no hit particle for those
                 particle = I3MCTreeUtils::GetParticlePtr(MCTree, photon.GetParticleID());
             }
-        
+
             // allocate the output vector if not already done
             if (!hits) hits = &(outputMCPESeriesMap->insert(std::make_pair(key, I3MCPESeries())).first->second);
 
@@ -526,31 +526,31 @@ void I3PhotonToMCPEConverter::DAQ(I3FramePtr frame)
                 const double bringForward = dot*(1.-DOMPancakeFactor_/DOMOversizeFactor_);
                 correctedTime += bringForward/photon.GetGroupVelocity();
             }
-            
+
             // add a new hit
             if(particle)
                 hits->push_back(I3MCPE(*particle));
             else
                 hits->push_back(I3MCPE());
             I3MCPE &hit = hits->back();
-            
+
             // fill in all information
             hit.time=correctedTime;
             hit.npe=1;
         }
-        
+
         if (hits) {
             // sort the photons in each hit series by time
             std::sort(hits->begin(), hits->end(), MCPETimeLess);
-            
+
             // keep track of the number of hits generated
             numGeneratedHits_ += static_cast<uint64_t>(hits->size());
         }
     }
-    
+
     // store the output I3MCPESeriesMap
     frame->Put(outputMCPESeriesMapName_, outputMCPESeriesMap);
-    
+
     // that's it!
     PushFrame(frame);
 }
@@ -561,8 +561,8 @@ void I3PhotonToMCPEConverter::Finish()
     I3SummaryServicePtr summary = context_.Get<I3SummaryServicePtr>();
     if (summary) {
         const std::string prefix = "I3PhotonToMCPEConverter_" + GetName() + "_";
-        
+
         (*summary)[prefix+"NumGeneratedHits"] = numGeneratedHits_;
     }
-    
+
 }
