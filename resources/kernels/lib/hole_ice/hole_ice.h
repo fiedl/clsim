84f75d6742f59dff10664e32c4a290ad10028f5c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-03-09 20:50:45 +0100

    removing previous hole-ice code
    This is the code that corrects `distancePropagated` and
    `distanceToAbsorption` based on relative hole-ice properties.
    
    This becomes obsolete through
    https://github.com/fiedl/hole-ice-study/issues/45.

--- resources/kernels/lib/hole_ice/hole_ice.h
+++ resources/kernels/lib/hole_ice/hole_ice.h
@@ -1,12 +1,5 @@
 #ifndef HOLE_ICE_H
 #define HOLE_ICE_H
 
-typedef struct HoleIceProblemParameters {
-  floating_t distance;
-  floating_t interaction_length_factor;
-  floating_t entry_point_ratio;
-  floating_t termination_point_ratio;
-  bool starts_within_hole_ice;
-} HoleIceProblemParameters_t;
 
 #endif
